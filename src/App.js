ca42f2fb1d96dd09d44f2f14a5f4922217e072ab
joyboy <pk5082093@gmail.com>
2025-03-04 09:56:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="body-border"></div>
+      <div className="glossy-container">
+        <h1>Welcome to My Movie App</h1>
+        <p>Enjoy a sleek, glossy background with a glass effect!</p>
+      </div>
+    </>
   );
 }
 
